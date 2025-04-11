55e34d10ed96dd2ce9de8adf336bd2c7e2f104ca
Shizun <Shizun@KULICH>
2025-04-11 17:24:34 +0500

    Разработанная функция AlertDialog. Osipov

--- app/src/main/java/com/example/orders_osipov/MainActivity.java
+++ app/src/main/java/com/example/orders_osipov/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.orders_osipov;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void AlertDialog(String title, String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("ОК", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {dialog.cancel();}
+                });
+
+        AlertDialog alter = builder.create();
+        alter.show();
+    }
+
+    public EditText fio;
+    public EditText phone;
+    public EditText adress;
+    public void OnArrange(View view)
+    {
+        fio = findViewById(R.id.fio);
+        phone = findViewById(R.id.phone);
+        adress = findViewById(R.id.adress);
+
+        if(fio.getText().length() == 0) AlertDialog("Уведомление", "Пожалуйста, укажите Фамилию, Имя и Отчество.");
+        else if(phone.getText().length() == 0) AlertDialog("Уведомление", "Пожалуйста, укажите номер телефона.");
+        else if(adress.getText().length() == 0) AlertDialog("Уведомление", "Пожалуйста, укажите адрес доставки.");
+        else {
+            AlertDialog("Уведомление", "Заказ оформлен.");
+        }
     }
 }
